58df5b206c25c7e61849a22057e0c1b4e2513645
IT21177828 <99233666+IT21177828@users.noreply.github.com>
2022-02-08 04:47:50 +0000

    I have completed exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
+  int mark1, mark2;
+  float average;
   
+  printf("Enter mark 1 : ");
+  scanf("%d", &mark1);
+  printf("Enter mark 2 : ");
+  scanf("%d", &mark2);
+  
+  average = (mark1 + mark2) / 2;
+  
+  printf("Average = %.2f\n", average);
+
   return 0;
 }
 
